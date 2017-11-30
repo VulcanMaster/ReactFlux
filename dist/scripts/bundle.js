ebc8abd1a5eb3f892ce7c753328adf8f52088139
Dmitry Blinkov <dmitry.blinkov@outlook.com>
2017-11-30 11:15:06 +0200

    Added control "authorList", which is used as child into the control "authorPage".

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -46322,19 +46322,8 @@ module.exports = About;
 "use strict";
 
 var React = require('react');
-var AuthorApi = require('../../api/authorApi');
 
-var Authors = React.createClass({displayName: "Authors",
-
-    getInitialState: function () {
-        return {
-            authors: []
-        };
-    },
-
-    componentWillMount: function () {
-        this.setState({ authors: AuthorApi.getAllAuthors() });
-    },
+var AuthorList = React.createClass({displayName: "AuthorList",
 
     render: function () {
         var createAuthorRow = function (author) {
@@ -46348,16 +46337,97 @@ var Authors = React.createClass({displayName: "Authors",
 
         return (
             React.createElement("div", null, 
-                React.createElement("h1", null, "Authors"), 
                 React.createElement("table", {className: "table"}, 
                     React.createElement("thead", null, 
                         React.createElement("th", null, "ID"), 
                         React.createElement("th", null, "Name")
                     ), 
                     React.createElement("tbody", null, 
-                        this.state.authors.map(createAuthorRow, this)
+                        this.props.authors.map(createAuthorRow, this)
                     )
                 )
+            )
+        );
+    }
+});
+
+module.exports = AuthorList;
+
+// "use strict";
+
+// var React = require('react');
+// var Router = require('react-router');
+// var Link = Router.Link;
+// var AuthorActions = require('../../actions/authorActions');
+// var toastr = require('toastr');
+
+// var AuthorList = React.createClass({
+// 	propTypes: {
+// 		authors: React.PropTypes.array.isRequired
+// 	},
+
+// 	deleteAuthor: function(id, event) {
+// 		event.preventDefault();
+// 		AuthorActions.deleteAuthor(id);
+// 		toastr.success('Author Deleted');
+// 	},
+
+// 	render: function() {
+// 		var createAuthorRow = function(author) {
+// 			return (
+// 				<tr key={author.id}>
+// 					<td><a href="#" onClick={this.deleteAuthor.bind(this, author.id)}>Delete</a></td>
+// 					<td><Link to="manageAuthor" params={{id: author.id}}>{author.id}</Link></td>
+// 					<td>{author.firstName} {author.lastName}</td>
+// 				</tr>
+// 			);
+// 		};
+
+// 		return (
+// 			<div>
+// 				<table className="table">
+// 					<thead>
+// 						<th></th>
+// 						<th>ID</th>
+// 						<th>Name</th>
+// 					</thead>
+// 					<tbody>
+// 						{this.props.authors.map(createAuthorRow, this)}
+// 					</tbody>
+// 				</table>
+// 			</div>
+// 		);
+// 	}
+// });
+
+// module.exports = AuthorList;
+
+},{"react":158}],163:[function(require,module,exports){
+"use strict";
+
+var React = require('react');
+var AuthorApi = require('../../api/authorApi');
+var AuthorList = require('./authorList');
+
+var Authors = React.createClass({displayName: "Authors",
+
+    getInitialState: function () {
+        return {
+            authors: []
+        };
+    },
+
+    componentDidMount: function () {
+        if (this.isMounted()) {
+            this.setState({ authors: AuthorApi.getAllAuthors() });
+        }
+    },
+
+    render: function () {
+        return (
+            React.createElement("div", null, 
+                React.createElement("h1", null, "Authors"), 
+                React.createElement(AuthorList, {authors: this.state.authors})
             )
         );
     }
@@ -46407,7 +46477,7 @@ module.exports = Authors;
 
 // module.exports = AuthorPage;
 
-},{"../../api/authorApi":159,"react":158}],163:[function(require,module,exports){
+},{"../../api/authorApi":159,"./authorList":162,"react":158}],164:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46437,7 +46507,7 @@ var Header = React.createClass({displayName: "Header",
 
 module.exports = Header;
 
-},{"react":158}],164:[function(require,module,exports){
+},{"react":158}],165:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
@@ -46458,7 +46528,7 @@ var Home = React.createClass({displayName: "Home",
 
 module.exports = Home;
 
-},{"react":158}],165:[function(require,module,exports){
+},{"react":158}],166:[function(require,module,exports){
 $ = jQuery = require('jquery');
 var React = require('react');
 var Home = require('./components/homePage');
@@ -46515,4 +46585,4 @@ var Header = require('./components/common/header');
 // // common js pattern
 // module.exports = App;
 
-},{"./components/about/aboutPage":161,"./components/authors/authorPage":162,"./components/common/header":163,"./components/homePage":164,"jquery":1,"react":158}]},{},[165]);
+},{"./components/about/aboutPage":161,"./components/authors/authorPage":163,"./components/common/header":164,"./components/homePage":165,"jquery":1,"react":158}]},{},[166]);
